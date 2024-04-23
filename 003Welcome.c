414d71546e39e7492f995769ed9d1652939da47f
RajaMahanty <rajamahanty2@gmail.com>
2024-04-23 19:47:40 +0530

    Raja

--- 003Welcome.c
+++ 003Welcome.c
@@ -5,6 +5,6 @@ int main(int argc, char const *argv[])
     char name[25];
     printf("Enter your name: ");
     scanf("%19s", name);
-    printf("Welcome %s to Balarampur");
+    printf("Welcome %s to Balarampur", name);
     return 0;
 }
