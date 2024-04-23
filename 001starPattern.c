de4a734ae04390b8780ca85e83e14508883e7f9e
RajaMahanty <rajamahanty2@gmail.com>
2024-04-23 19:09:32 +0530

    Third  commit

--- 001starPattern.c
+++ 001starPattern.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    
+    printf("Right Half Pyramid\n");
+    printf("*\n");
+    printf("* *\n");
+    printf("* * *\n");
+    printf("* * * *\n");
+    printf("* * * * *\n");
+
+
+
     return 0;
 }
