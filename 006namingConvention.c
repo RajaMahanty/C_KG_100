b8d993dbaf597a2b281721686df59d087a1d48fa
RajaMahanty <rajamahanty2@gmail.com>
2024-04-24 15:55:16 +0530

    updated repo by Raja

--- 006namingConvention.c
+++ 006namingConvention.c
@@ -2,6 +2,10 @@
 
 int main()
 {
-    
+    char first_name[25] = "Raja";
+    char last_name[25] = "Mahanty";
+    int age = 21;
+
+    printf("First name: %s\nSecond name: %s\nAge: %d",first_name,last_name,age);
     return 0;
 }
