a910d8b71f2ef4f3c27cdca7fac060bbae8d5c62
RajaMahanty <rajamahanty2@gmail.com>
2024-05-05 19:46:04 +0530

    repo updated on 19:45 05-05-2024 by RM

--- 026evenOddTernary.c
+++ 026evenOddTernary.c
@@ -4,15 +4,7 @@ int main(int argc, char const *argv[])
     int num;
     printf("Enter numer: ");
     scanf("%d", &num);
-    num = num % 2 ? 0 : 1;
-    if (num == 0)
-    {
-        printf("Odd\n");
-    }
-    else
-    {
-        printf("Even\n");
-    }
-
+    num = num % 2 ? printf("Odd\n")
+                  : printf("Even\n");
     return 0;
 }
